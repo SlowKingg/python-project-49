d7a8ed2b0b7c38704cd011fd5a3b9d23c913dd92
SlowKing <slowkinggg@vk.com>
2025-07-09 21:22:27 +0700

    Added get_answer function in cli.py to get answer from user.

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,13 +1,23 @@
 import prompt
 
 
+def get_answer() -> str | None:
+    """Gets answer from user.
+
+    Returns:
+        str | None: Answer.
+    """
+
+    return prompt.string("Your answer: ")
+
+
 def welcome_user() -> str | None:
     """Gets user name and welcomes him.
 
     Returns:
         str | None: Name of user.
     """
-    
+
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
     return name
