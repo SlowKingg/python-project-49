1e884419417914c57657d1862b1261a88e1bb45a
SlowKing <slowkinggg@vk.com>
2025-07-09 17:59:40 +0700

    Changed welcome_user function to return name of user.

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,13 @@
 import prompt
 
 
-def welcome_user() -> None:
-    """Gets user name and welcomes him."""
+def welcome_user() -> str | None:
+    """Gets user name and welcomes him.
+
+    Returns:
+        str | None: Name of user.
+    """
+    
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
+    return name
