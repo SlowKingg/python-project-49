9324fe455a05762df30b6043b867292341e2c9c4
SlowKing <slowkinggg@vk.com>
2025-07-09 21:23:20 +0700

    Implemented the game "Evenness Check".

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,34 @@
+from random import randint
+
+from brain_games.cli import get_answer, welcome_user
+
+
+def main() -> None:
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    for _ in range(3):
+        question_number = randint(1, 100)
+        print(f"Question: {question_number}")
+        right_answer = "yes" if question_number % 2 == 0 else "no"
+        user_answer = get_answer()
+
+        if right_answer == user_answer:
+            print("Correct!")
+        else:
+            print(
+                (
+                    f"'{user_answer}' is wrong answer ;(.\n"
+                    f"Correct answer was '{right_answer}'.\n"
+                    f"Let's try again, {name}!"
+                )
+            )
+            break
+    else:
+        print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
